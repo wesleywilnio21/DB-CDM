8aa318cc6b6cb42e22782e2e88dcb56690f9470b
Wesley Wilnio <wesleywilnio21@gmail.com>
2026-06-08 22:44:39 +0700

    Fix Supabase client & add Vercel output config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = https://jmzbgxiqijhkfpxjxruf.supabase.co
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImptemJneGlxaWpoa2ZweGp4cnVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0ODIyOTksImV4cCI6MjA5NjA1ODI5OX0.ZP0CT2y_B6bZzQGLzMnpuMZLea7qsBuXYSiCdeTdG2k
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
