9d6012635436ca5fe98ed683e856ff397501e2d8
Wesley Wilnio <wesleywilnio21@gmail.com>
2026-07-23 18:53:51 +0700

    Init connection to Hostinger

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,10 @@ export default defineConfig({
     manifest: true,
     outDir: 'public/build',
     rollupOptions: {
-      input: {
-        app: 'resources/js/app.js',
-      },
+      input: [
+        'resources/js/app.js',
+        'resources/css/app.css'
+      ],
     },
   },
 });
